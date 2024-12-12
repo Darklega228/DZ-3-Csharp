6321a2fceae9ad8f810bd270f5227d849f53b1b8
Darklega <collplayer20@gmail.com>
2024-12-12 21:07:02 +0200

    DZ C#

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DZ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество ступеней лесенки: ");
+            int steps = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= steps; i++)
+            {
+                Console.WriteLine(new string('*', i));
+            }
+
+            Console.WriteLine("Лесенка завершена!");
+            Console.ReadKey();
+        }
+    }
+}
